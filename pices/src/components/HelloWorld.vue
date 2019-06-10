c52a138250de9fa9418729c1f639a832c7743c82
LXY <605386251@qq.com>
2019-06-10 10:30:33 +0800

    init

---
+++ pices/src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="warper">
+    <h1>
+      首页
+    </h1>
+    
+  </div>
+</template>
+<script>
+export default {
+  props:{
+
+  },
+  components:{
+
+  },
+  data(){
+    return {
+
+    }
+  },
+  computed:{
+
+  },
+  methods:{
+
+  },
+  created(){
+
+  },
+  mounted(){
+
+  }
+}
+</script>
+<style scoped lang="">
+
+</style>
